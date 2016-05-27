a82723e8950ba435562c9bab784f1dd22a127b30
Christoph Wagner <github@pandawhisperer.net>
2016-05-27 13:36:18 -0700

    Setting up routing and layouts

---
+++ imports/startup/client/index.js
@@ -0,0 +1,6 @@
+// routes
+import './routes.js';
+
+// templates
+import '../../ui/layouts/main.html';
+import '../../ui/pages/home.html';
---
+++ imports/startup/client/routes.js
@@ -0,0 +1,9 @@
+import { FlowRouter } from 'meteor/kadira:flow-router'
+import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+
+FlowRouter.route('/', {
+  name: 'home',
+  action() {
+    BlazeLayout.render("mainLayout", { content: "home" });
+  }
+});
