b4965bc281ab0dac9d50dcf629306b89d6e6b3a1
Christoph Wagner <github@pandawhisperer.net>
2016-06-09 13:03:36 -0700

    Adding price level, photos, and address to PlaceDetail component

--- imports/ui/components/PlaceDetail/PlaceDetail.js
+++ imports/ui/components/PlaceDetail/PlaceDetail.js
@@ -3,4 +3,14 @@ import { Template } from 'meteor/templating';
 import './PlaceDetail.html';
 
 Template.PlaceDetail.helpers({
-})
+  photoUrl(photo) {
+    return 'getUrl' in photo ? photo.getUrl({
+      maxWidth: 200, maxHeight: 200
+    }) : null;
+  },
+
+  priceLevel(place) {
+    const priceLevel = place.price_level;
+    return new Array(priceLevel || 0).fill(1);
+  }
+});
--- imports/ui/components/PlaceDetail/client/PlaceDetail.tests.js
+++ imports/ui/components/PlaceDetail/client/PlaceDetail.tests.js
@@ -8,10 +8,28 @@ import '../PlaceDetail.js';
 
 describe('PlaceDetail component', function() {
   const place = {
-    name: "Moe's Tavern"
+    name: "Moe's Tavern",
+    price_level: 2,
+    photos: [{
+      getUrl() { return 'moes-tavern.jpg' }
+    }],
+    vicinity: "Springfield U.S.A."
   };
 
   it('shows a header with the place name', function() {
     ensureElement('PlaceDetail', { place }, "h2:contains(Moe's Tavern)");
+  });
+
+  it('shows the address of the place', function() {
+    ensureElement('PlaceDetail', { place }, ".address:contains(Springfield U.S.A.)");
+  });
+
+  it('shows the price level if available', function() {
+    ensureElement('PlaceDetail', { place }, "span.price-level .fa-dollar", 2);
+  });
+
+  it('shows photos of the place', function() {
+    ensureElement('PlaceDetail', { place }, "h4:contains(Photos)");
+    ensureElement('PlaceDetail', { place }, "img[src='moes-tavern.jpg']");
   });
 });
